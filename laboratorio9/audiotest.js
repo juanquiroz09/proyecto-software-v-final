c6bb716f026e7dba32f231f09a90118c3ad6dc6d
juanquiroz09 <juan.quiroz1009@gmail.com>
2022-07-13 13:31:13 -0500

    first commit

---
+++ laboratorio9/audiotest.js
@@ -0,0 +1,27 @@
+function Reproducir(){
+var audio = document.getElementById('mi-audio');
+audio.play();
+}
+function Pausar(){
+var audio = document.getElementById('mi-audio');
+audio.pause();
+}
+function CrearAudioDinamicamente() {
+var audio = document.createElement("audio");
+audio.src = "audiotest2.mp3";
+audio.addEventListener('ended', function () {
+setTimeout(function () { audio.play(); }, 500);
+}, false);
+audio.play();
+}
+function VerOtrasPropiedades() {
+var audio = document.getElementById('mi-audio');
+var tiempo = audio.currentTime; 
+var duracion = audio.duration; 
+var estadored = audio.networkState; 
+var estado = audio.readyState;
+alert("Tiempo actual de reproducción: "+tiempo); 
+alert ("Duracion total del audio: "+duracion); 
+alert ("Estado de red: "+estadored); 
+alert("Estado de carga: "+estado);
+}
